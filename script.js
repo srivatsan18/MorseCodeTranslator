1fd6ecff6f6699698279715ad47d3265e75ef059
Srivatsan m <srivatsan4you@gmail.com>
2021-01-30 11:45:07 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var inputtxt=document.querySelector("#txt");
+var out=document.querySelector("#outti");
+var btntranslate=document.querySelector("#btn-translate");
+var url="https://api.funtranslations.com/translate/morse.json";
+out.innerText="Zazooooooooooo......................."
+function urlgetter(text){
+  return url+ "?"+"text="+ text;
+
+}
+function errorhandler(error){
+  console.log("error occured",error);
+}
+function clickhandler()
+{
+   var textt=inputtxt.value;
+  fetch(urlgetter(textt))
+  .then(response => response.json())
+  .then(json =>{
+    var outtext=json.contents.translated
+    out.innerText=outtext;
+
+  })
+  .catch(errorhandler)
+}
+btntranslate.addEventListener("click", clickhandler)
+
+
+
+
